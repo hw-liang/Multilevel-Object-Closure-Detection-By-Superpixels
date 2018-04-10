f97307c82be822619b58087c8275847f68b84809
Han <hm6502163@gmail.com>
2018-04-10 15:19:27 -0400

    Implemented the selective closure masking in the ClosureMain.m file.

--- ClosureMain.m
+++ ClosureMain.m
@@ -9,7 +9,7 @@
 %   edge_threshold  - Pb edge threshold (smaller values result in more
 %                     solutions)
 %   sup_method      - The method used to generate superpixels.
-%                     Valid values: 'ncuts', 'turbo', 'slic'
+%                     Valid values: 'ncuts', 'turbo' (default)
 %   Example: ClosureMain(img_filename, '.', 100, 10, 0.05, 'turbo');
 %            This would open the image in img_file, extract 100 superpixels
 %            using the Turbopixels method, use an edge threshold of 0.05,
@@ -19,8 +19,9 @@
 function ClosureMain(img_filename, output_dir, num_sups, num_solutions, ...
     edge_thresh, sup_algorithm)
     
+    %% setup files
     core_name = img_filename(1:end-4);
-    if (~exist(core_name,'file'))
+    if (~exist(core_name,'dir'))
         mkdir(core_name)
     end
     
@@ -31,8 +32,16 @@ function ClosureMain(img_filename, output_dir, num_sups, num_solutions, ...
     end
     
     img = im2double(imread(img_filename));  % read a image and convert it to double precision
-
-    % Compute Pb
+    img_copy = img;  % get a copy of the original image
+    % Add base case here
+    [m,n] = size(img);
+    if (m*n) < 1000
+        % If the total size of the image is less than 1000 pixels, return.
+        disp('The input image is too small. The function does not continue.');
+       return; 
+    end
+    
+    %% Compute Pb
     disp('Computing Pb');
     pb_file = [core_name,'/',img_filename(1:end-4),'_pb.mat'];  % make a new name for the data file for the image
     if (~exist(pb_file, 'file'))  % if the pb file does not exist
@@ -68,7 +77,9 @@ function ClosureMain(img_filename, output_dir, num_sups, num_solutions, ...
     else
         load(image_data_file);
     end
-
+    
+    
+    %% Compute superpixels
     disp('Computing Superpixels');
     superpixels_file = [core_name,'/',img_filename(1:end-4),'_num_sups_',num2str(num_sups),'.seg'];
     if (~exist(superpixels_file, 'file'))
@@ -95,15 +106,28 @@ function ClosureMain(img_filename, output_dir, num_sups, num_solutions, ...
     end
     sup_image = CleanSupImage(sup_image);  % fill the empty pixel locations
 
+    
+    %% Parametric Maxflow
     disp('Looking for closures using parametric maxflow');
     % input: file name, superpixel labels, pb edge data, denominator for
     % the cost function, the max number of solutions, the selected 
-    % superpixels, edge threshold for support.
-    % output: X is the first selection of superpixels, Xs contains a total
-    % of num_solutions of selections of superpixels, cost is the result of the minimum
+    % superpixels (limit the range?) edge threshold for support.
+    % output: X is the first solution's selected pixels, Xs might be the
+    % selected pixles for all possible solutions, cost is the result of the minimum
     % cost function, the sup-image is the label image for superpixels.
+    
+    [X2, Xs2, cost, sup_image] = SuperpixelClosureGrouping(img_filename, sup_image, ...
+        image_data, 'area', num_solutions, [], edge_thresh);  % call the superpixel grouping on the ground level
+    
+    % test code:
+    unselected_sup = find(X2);  % find the selected superpixels
+    for i = 1:numel(unselected_sup)
+        % for each element index in the selected_sup
+        sup_image(sup_image == unselected_sup(i)) = 0;  % make them 0.
+    end
+    
     [X, Xs, cost, sup_image] = SuperpixelClosureGrouping(img_filename, sup_image, ...
-        image_data, 'area', num_solutions, [], edge_thresh);
+        image_data, 'area', num_solutions, [], edge_thresh);  % call the superpixel grouping on the ground level
     
     disp('Postprocessing the results');
     Xs = ChooseLargestSupComponent(sup_image, Xs);  % choose a group of large number of superpixels
@@ -124,17 +148,48 @@ function ClosureMain(img_filename, output_dir, num_sups, num_solutions, ...
     all_0 = sum(Xs, 1) == 0;
     Xs(:, all_0) = [];
 
+    %% Save the solutions out to files
     disp('Saving solutions');
     % Save the figure images into a files
     [pathstr, name, ext] = fileparts(img_filename);
     results_img_file = [output_dir,'/',core_name,'/',name,'_multiplesolutions.jpg'];  % the file name to hold multiple solutions
-    % the number of solutions might be smaller than the num_solutions
-    s = min([size(Xs,2), num_solutions]);  
-    % get multiple solutions into an 3D array,results_img shows all the
-    % results in one picture.
-    results_img = DrawSuperpixelsAreaIterationsSingleFigure(img, sup_image, Xs(:,1:s));  
+
+    s = min([size(Xs,2), num_solutions]);  % the number of solutions might be smaller than the num_solutions
+    results_img = DrawSuperpixelsAreaIterationsSingleFigure(img, sup_image, Xs(:,1:s));  % get multiple solutions into an 3D array
     imwrite(results_img, results_img_file, 'jpg');  % write the image to the file
 
+    % Crop the image here, use the first solution as the target
+    % Use the original image, superpixel image, and the binary labels
+%     [m,n,c] = size(img);
+%     h_min = intmax; h_max = intmin; v_min = intmax; v_max = intmin;
+%     X_labels = Xs(:,1);
+%     X_labels = X;
+%     for i = 1:m
+%         for j = 1:n
+%             label = sup_image(i,j);
+%             if (label > 0)
+%                if X_labels(label) == 1
+%                     % in the group of selected superpixels
+%                     if (j < h_min)
+%                         h_min = j;
+%                     end
+%                     if (j > h_max)
+%                         h_max = j;
+%                     end
+%                     if (i < v_min)
+%                         v_min = i;
+%                     end
+%                     if (i > v_max)
+%                         v_max = i;
+%                     end
+%                end
+%             end
+%         end
+%     end
+%     v_min, h_min, v_max, h_max
+%     test_crop = imcrop(img, [h_min v_min (h_max-h_min) (v_max-v_min)]); 
+%     imshow(test_crop);
+%     imwrite(test_crop, 'test.jpg');
     for sol = 1:s
         % For each solution, write the foreground (white and black) to an
         % image.
@@ -142,3 +197,4 @@ function ClosureMain(img_filename, output_dir, num_sups, num_solutions, ...
         fg = SupValueImage_MEX(sup_image, double(Xs(:,sol)));  % get the foreground image
         imwrite(fg, results_img_file, 'jpg');  % the white and black images
     end
+%     ClosureMain('test.jpg', '.', 100, 10, 0.05, 'slic');
--- SuperpixelClosureGrouping.m
+++ SuperpixelClosureGrouping.m
@@ -26,12 +26,14 @@ function [X, Xs, cost, sup_image, costs] = SuperpixelClosureGrouping(img_filenam
         
     if (exist('selected_sups', 'var') && ~isempty(selected_sups))
         % If there are selected superpixels
-        num_sups = sum(selected_sups);
+        num_sups = sum(selected_sups);  % total number of selected superpixels
         adjmat = adjmat(selected_sups, selected_sups);  % make a adjacent matrix only for the selected superpixels
-        sup_image(SupValueImage_MEX(sup_image, double(selected_sups)) == 0) = 0;  % make the selected pixels unselected in the label image
+        % SupValueImage_MEX will return the foreground (white) and
+        % background image (black)
+        sup_image(SupValueImage_MEX(sup_image, double(selected_sups)) == 0) = 0;  % make the previous selected pixels to be 0
         % [b,m,new_sups] = unique(sup_image(:));
-        new_sups = unique(sup_image(:)); % These two lines may not be needed.
-        sup_image = reshape(new_sups, size(sup_image)) - 1;
+%         new_sups = unique(sup_image(:)); % These two lines may not be needed.
+%         sup_image = reshape(new_sups, size(sup_image)) - 1;
     end    
 
     % Compute all the edge fragments between superpixels
@@ -117,8 +119,14 @@ function [X, Xs, cost, sup_image, costs] = SuperpixelClosureGrouping(img_filenam
         fragments, segment_fragments, fragment_segments, ...
         poly_angle, poly_sq_curv, image_data, edge_thresh, edge_affinity);
     G = P - E;
+    if (size(G) ~= size(gap_multiplier))
+        gap_multiplier(end,:) = [];
+        gap_multiplier(:, end) = [];
+        affinity(end,:) = [];
+        affinity(:, end) = [];
+    end
     G = G .* gap_multiplier; % G is the edge gap matrix
-    D = sum(affinity,2);  % a column with sums of rows
+    D = sum(affinity,2);  % a column with sums of rows; each element is the area of a superpixel
 %     X = MinimizeRatioBinary(double(rand(num_sups,1)>0.5),G, affinity);
     
     % Can call closure extraction recursively on each solution and its
@@ -128,10 +136,11 @@ function [X, Xs, cost, sup_image, costs] = SuperpixelClosureGrouping(img_filenam
             Xs = X;
         else
             Xs1 = [];
-            if (sum(X > 0.5) > 1)
-                [X1, Xs1, cost1, sup_image1] = SuperpixelClosureGrouping(img_filename, sup_image, image_data, num_solutions-1, X > 0.5);
-                repetitions = sum((Xs1 == repmat(X, 1, size(Xs1, 2))), 1) == numel(X);
-                Xs1 = Xs1(:, ~repetitions);
+            if (sum(X > 0.5) > 1)  % if there is grouping of superpixels
+                [X1, Xs1, cost1, sup_image1] = SuperpixelClosureGrouping(img_filename, ...
+                    sup_image, image_data, num_solutions-1, X > 0.5);  % recursively call the self function
+                repetitions = sum((Xs1 == repmat(X, 1, size(Xs1, 2))), 1) == numel(X);   % find the repetitions
+                Xs1 = Xs1(:, ~repetitions);  % get the groupings not repeated
             end
             Xs2 = [];
             if (sum(X < 0.5) > 1)
@@ -148,14 +157,17 @@ function [X, Xs, cost, sup_image, costs] = SuperpixelClosureGrouping(img_filenam
         end
     else
         % if recursion is not on; default option
+        % use parametric maxflow to find the grouping the the superpixels
+        % Xs is the matrix with each column as a grouping; lambda is a
+        % vector with all possible lambda values.
         [Xs,lambda] = ParametricMaxflow_MEX([D';diag(G)'], 2*G, -1, 0, 100);
         % The second argument is a function pointer.
-        e = EvaluateMultipleSolutions(Xs, @CostGapOverAffinity, G, diag(D));
+        e = EvaluateMultipleSolutions(Xs, @CostGapOverAffinity, G, diag(D));  % Computes the costs of multiple solutions given a cost function
         e(isnan(e) | isinf(e)) = inf;
-        [costs,idx] = sort(e);
-        Xs = Xs(:,idx(1:min(size(Xs,2), num_solutions)));
-        costs = costs(1:min(size(Xs,2), num_solutions));
-        X = Xs(:,1);  % pick the first column
+        [costs,idx] = sort(e);  % sort the cost results
+        Xs = Xs(:,idx(1:min(size(Xs,2), num_solutions)));  % ordered the groupings by the ascending cost order
+        costs = costs(1:min(size(Xs,2), num_solutions));  % also order the costs
+        X = Xs(:,1);  % pick the first column as the best grouping
     end
     
     if (display_on)  % If the display is on, it will show the results.
